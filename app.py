d49d65d4fa703b2bc0eaede8a6b69d23f6b64421
malcolmfrsr <malcolmfrsr@gmail.com>
2019-03-28 00:26:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import time
 import redis
 from flask import Flask
 
-# test 1   dfdfsssss
+# test 1   dfdfssssffffffffffffffffs
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
 
