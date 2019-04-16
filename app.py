b9a670c02f69b49e7d09bd8a17142002b8c6e2cc
malcolmfrsr <malcolmfrsr@gmail.com>
2019-04-16 13:24:35 +0200

    d
    d

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import time
 import redis
 from flask import Flask
 
-# got to triggfer that versiddddddddddon.
+# got to triggfer that versidddddddddddddon.
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
 
-# whatfffdddddddddddf
+# whatffddddfdddddddddddf
 def get_hit_count():
     retries = 5
     while True:
